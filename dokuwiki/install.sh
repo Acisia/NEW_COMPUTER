9cd0dd4446bd96447e72c57ec829ff2e41d9a749
Louis DAUBIGNARD <l.daubignard@acisia.com>
2015-11-04 01:35:08 +0100

    add plugins

--- dokuwiki/install.sh
+++ dokuwiki/install.sh
@@ -33,15 +33,27 @@ printMessageTo  "             DOWNLOAD dokuwiki			" "2"
 curl -O http://download.dokuwiki.org/out/$DOKUWIKIVER
 printMessageTo  "             DECOMPRESSION Archive			" "2" 
 tar -xzvf $DOKUWIKIVER
+
+
+printMessageTo  "             PLUGINS	note		" "2" 
+cd "$PATHROOT/dokuwiki/lib/plugins"
+# https://www.dokuwiki.org/plugin:note
+# https://www.dokuwiki.org/plugin:color
+# https://www.dokuwiki.org/plugin:dw2pdf
+cp -R $PATHROOT/plugins/* $PATHROOT/dokuwiki/lib/plugins
+
+cd $PATHROOT
 printMessageTo  "             RENOMMER			" "2" 
 mv -f "dokuwiki" "$PROJECTNAME"
+
 printMessageTo  "             DEPLACER			" "2" 
 checkPathDst "$PROJECTNAME" "Chemin DOKUWIKI $PROJECTNAME"
 checkPathDst "$PATHWWW$PROJECTNAME" "Chemin web $PATHWWW$PROJECTNAME"
 cp -R dokuwiki/* $PROJECTNAME
 cp -R dokuwiki/* $PATHWWW$PROJECTNAME
+
 printMessageTo  "             NETTOYAGE			" "2" 
 rm -r dokuwiki
 printMessageTo  "             TEST PAGE INSTALL			" "2" 
-start iexplorer.exe "http://localhost/toyota/install.php?l=fr&d[title]=$PROJECTNAME&d[superuser]=$USER_LOGIN&d[fullname]=$USER_PRENOM $USER_NOM&d[email]=$USER_ADRESSEMAIL&d[password]$USER_PASSWORD&d[confirm]=$USER_PASSWORD&d[policy]=2&d[pop]=0"
+start iexplore.exe "http://localhost/toyota/install.php?l=fr&d[title]=$PROJECTNAME&d[superuser]=$USER_LOGIN&d[fullname]=$USER_PRENOM $USER_NOM&d[email]=$USER_ADRESSEMAIL&d[password]$USER_PASSWORD&d[confirm]=$USER_PASSWORD&d[policy]=2&d[pop]=0"
 echo "    FIN" 
